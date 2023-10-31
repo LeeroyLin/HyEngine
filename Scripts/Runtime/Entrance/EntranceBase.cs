2699e0ffc335bf6384bb5c7187907334dc412011
黎旭 <897313750@qq.com>
2023-10-31 11:53:00 +0800

    feat: 入口

---
+++ Scripts/Runtime/Entrance/EntranceBase.cs
@@ -0,0 +1,20 @@
+﻿namespace Engine.Scripts.Runtime.Entrance
+{
+    public abstract class EntranceBase
+    {
+        public void Start()
+        {
+            OnInit();
+            OnStart();
+        }
+        
+        public void Tick(float dt)
+        {
+            OnTick(dt);
+        }
+
+        protected abstract void OnInit();
+        protected abstract void OnStart();
+        protected abstract void OnTick(float dt);
+    }
+}
