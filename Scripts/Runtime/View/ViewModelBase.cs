9cdd129d7e30194583d60e4bc89cfc5cff03924b
黎旭 <897313750@qq.com>
2023-11-07 19:15:40 +0800

    fix: VM包装事件方法

--- Scripts/Runtime/View/ViewModelBase.cs
+++ Scripts/Runtime/View/ViewModelBase.cs
@@ -1,4 +1,5 @@
-﻿using Engine.Scripts.Runtime.Event;
+﻿using System;
+using Engine.Scripts.Runtime.Event;
 
 namespace Engine.Scripts.Runtime.View
 {
@@ -12,5 +13,35 @@ namespace Engine.Scripts.Runtime.View
         }
 
         public abstract void Init();
+        
+        protected abstract void OnRegGameEvents();
+
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <typeparam name="T"></typeparam>
+        public void Reg<T>(Action<T> callback) where T : EventDataBase
+        {
+            EventGroup.Reg(callback);
+        }
+        
+        /// <summary>
+        /// 同步广播
+        /// </summary>
+        /// <param name="data"></param>
+        public void Broadcast(EventDataBase data)
+        {
+            EventGroup.Broadcast(data);
+        }
+
+        /// <summary>
+        /// 异步同步广播
+        /// </summary>
+        /// <param name="data"></param>
+        public void BroadcastAsync(EventDataBase data)
+        {
+            EventGroup.BroadcastAsync(data);
+        }
     }
 }
