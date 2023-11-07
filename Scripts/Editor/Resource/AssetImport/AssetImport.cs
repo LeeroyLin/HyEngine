dc438a8cee56c754b0946d884b08d96abb92108b
黎旭 <897313750@qq.com>
2023-11-07 17:17:48 +0800

    feat: 处理FGUI导出文件分离

---
+++ Scripts/Editor/Resource/AssetImport/AssetImport.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using UnityEditor;
+
+namespace Engine.Scripts.Editor.Resource.AssetImport
+{
+    public class AssetImport : AssetPostprocessor
+    {
+        private static string UI_DIR = $"Assets\\BundleAssets\\UI".Replace("/", "\\");
+        
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
+            string[] movedFromAssetPaths)
+        {
+            foreach (var p in importedAssets)
+            {
+                // 非文件
+                if (!File.Exists(p))
+                    continue;
+
+                // FGUI导出处理
+                FGUIExportPost(p);
+            }
+            
+            AssetDatabase.Refresh();
+        }
+
+        /// <summary>
+        /// FGUI导出处理
+        /// </summary>
+        /// <param name="p"></param>
+        private static void FGUIExportPost(string p)
+        {
+            var path = p.Replace("/", "\\");
+
+            var dir = Path.GetDirectoryName(path);
+
+            var extension = Path.GetExtension(path);
+
+            if (dir == UI_DIR)
+            {
+                if (extension == ".bytes")
+                {
+                    MoveDesc(path, dir);
+                }
+                else if (extension == ".png")
+                {
+                    MovePng(path, dir);
+                }
+            }
+        }
+        
+        private static void MoveDesc(string path, string dir)
+        {
+            var fileName = Path.GetFileName(path);
+            var newDir = $"{dir}\\Desc";
+            
+            MakeSureDir(newDir);
+
+            var newPath = $"{newDir}\\{fileName}";
+            
+            File.Move(path, newPath);
+        }
+        
+        private static void MovePng(string path, string dir)
+        {
+            var fileName = Path.GetFileName(path);
+            var pkgName = fileName.Split("_")[0];
+            var newDir = $"{dir}\\Res\\{pkgName}";
+            
+            MakeSureDir(newDir);
+            
+            var newPath = $"{newDir}\\{fileName}";
+            
+            File.Move(path, newPath);
+        }
+
+        private static void MakeSureDir(string dir)
+        {
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+        }
+    }
+}
