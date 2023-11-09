7688aea3ecb6cd14c656f5388c53cdce04836e7b
黎旭 <897313750@qq.com>
2023-11-09 13:50:50 +0800

    fix: System基类增加事件方法

--- Scripts/Runtime/System/SystemBase.cs
+++ Scripts/Runtime/System/SystemBase.cs
@@ -1,11 +1,17 @@
-﻿namespace Engine.Scripts.Runtime.System
+﻿using System;
+using Engine.Scripts.Runtime.Event;
+
+namespace Engine.Scripts.Runtime.System
 {
     public abstract class SystemBase<T> : ISystem where T : SystemModelBase, new()
     {
         public T SystemModel { get; private set; }
+        public EventGroup EventGroup { get; private set; }
 
         public SystemBase()
         {
+            EventGroup = new EventGroup(EEventGroup.GameLogic);
+            
             SystemModel = new T();
             
             SystemModel.Init();
@@ -13,16 +19,51 @@
         
         public void Enter()
         {
+            OnRegGameEvents();
+            
             OnEnter();
         }
 
         public void Exit()
         {
+            EventGroup.ClearCurrentAllEvents();
+            
             OnExit();
         }
 
         protected abstract void OnEnter();
 
         protected abstract void OnExit();
+        
+        protected abstract void OnRegGameEvents();
+        
+
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <typeparam name="T"></typeparam>
+        public void Reg<T>(Action<T> callback) where T : IEventData
+        {
+            EventGroup.Reg(callback);
+        }
+        
+        /// <summary>
+        /// 同步广播
+        /// </summary>
+        /// <param name="data"></param>
+        public void Broadcast(IEventData data)
+        {
+            EventGroup.Broadcast(data);
+        }
+
+        /// <summary>
+        /// 异步同步广播
+        /// </summary>
+        /// <param name="data"></param>
+        public void BroadcastAsync(IEventData data)
+        {
+            EventGroup.BroadcastAsync(data);
+        }
     }
 }
