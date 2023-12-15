cf9ea907b3b34fccc937d2ffc0b506bccbd09870
黎旭 <897313750@qq.com>
2023-12-15 18:15:27 +0800

    fix: 场景基类增加进入检测（异步进入）

--- Scripts/Runtime/Scene/SceneBase.cs
+++ Scripts/Runtime/Scene/SceneBase.cs
@@ -13,6 +13,8 @@ namespace Engine.Scripts.Runtime.Scene
         
         public EventGroup EventGroup { get; private set; }
         
+        public bool IsEntered { get; private set; }
+        
         public SceneBase(string key)
         {
             Key = key;
@@ -27,13 +29,18 @@ namespace Engine.Scripts.Runtime.Scene
 
         public void Enter(SceneArgsBase args = null)
         {
-            PreLoadUIPkg();
-            
-            OnRegGameEvents();
+            if (IsEntered)
+                return;
 
-            StartSystems();
+            void Handler()
+            {
+                DoEnter(args);
+            }
             
-            OnEnter(args);
+            bool isEnterNow = OnCheckEnter(Handler, args);
+
+            if (isEnterNow)
+                Handler();
         }
 
         public void Exit()
@@ -43,14 +50,44 @@ namespace Engine.Scripts.Runtime.Scene
             CloseSystems();
             
             OnExit();
+
+            IsEntered = false;
         }
 
         protected abstract void OnInit();
+        
+        /// <summary>
+        /// 检测是否立即进入场景
+        /// 返回false，不会立即进入场景
+        /// </summary>
+        /// <param name="doEnterHandler">用于主动进入场景的方法</param>
+        /// <param name="args"></param>
+        /// <returns>true：立即进入场景 false：等待主动进入场景</returns>
+        protected abstract bool OnCheckEnter(Action doEnterHandler, SceneArgsBase args = null);
         protected abstract string[] OnGetPreLoadUIPkg();
         protected abstract void OnEnter(SceneArgsBase args = null);
         protected abstract void OnExit();
         protected abstract ISystem[] OnGetSystems();
         protected abstract void OnRegGameEvents();
+
+        /// <summary>
+        /// 执行进入
+        /// </summary>
+        void DoEnter(SceneArgsBase args)
+        {
+            if (IsEntered)
+                return;
+
+            IsEntered = true;
+
+            PreLoadUIPkg();
+            
+            OnRegGameEvents();
+
+            StartSystems();
+            
+            OnEnter(args);
+        }
 
         void PreLoadUIPkg()
         {
@@ -88,6 +125,16 @@ namespace Engine.Scripts.Runtime.Scene
         protected void Reg<T>(Action<T> callback) where T : IEventData
         {
             EventGroup.Reg(callback);
+        }
+
+        /// <summary>
+        /// 取消注册
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <typeparam name="T"></typeparam>
+        protected void UnReg<T>(Action<T> callback) where T : IEventData
+        {
+            EventGroup.UnReg(callback);
         }
         
         /// <summary>
