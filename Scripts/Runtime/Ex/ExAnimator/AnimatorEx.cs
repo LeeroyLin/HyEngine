55694771280c98564c124bfe09dc3a24c90579f9
黎旭 <897313750@qq.com>
2024-01-26 15:09:52 +0800

    feat: 动画播放回调

---
+++ Scripts/Runtime/Ex/ExAnimator/AnimatorEx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Engine.Scripts.Runtime.Ex.ExAnimator
+{
+    public static class AnimatorEx
+    {
+        public static void Play(this Animator anim, string animName, Action callback)
+        {
+            var data = anim.gameObject.GetComponent<AnimatorExData>();
+            if (data == null)
+                data = anim.gameObject.AddComponent<AnimatorExData>();
+
+            data.PlayWithCallback(animName, callback);
+        }
+    }
+}
