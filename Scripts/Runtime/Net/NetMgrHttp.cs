4abe30d789da73d8948e2941ee0d68427be43e42
黎旭 <897313750@qq.com>
2024-07-01 16:27:48 +0800

    fix: 修改http

--- Scripts/Runtime/Net/NetMgrHttp.cs
+++ Scripts/Runtime/Net/NetMgrHttp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Engine.Scripts.Runtime.Utils;
+using LC.Newtonsoft.Json;
 using UnityEngine.Networking;
 
 namespace Engine.Scripts.Runtime.Net
@@ -66,8 +67,53 @@ namespace Engine.Scripts.Runtime.Net
             Dictionary<string, string> searchStrData = null)
         {
             url = GetUrlWithSearchStrData(url, searchStrData);
+
+            var jsonStr = JsonConvert.SerializeObject(formData);
             
             var webRequest = UnityWebRequest.Post(url, formData);
+            
+            if (headerData != null)
+                foreach (var data in headerData)
+                    webRequest.SetRequestHeader(data.Key, data.Value);
+
+            try
+            {
+                await webRequest.SendWebRequest();
+            }
+            catch (Exception e)
+            {
+                _log.Error($"[byte[]] Http 'post' request to url:'{url}' error. err:'{e.Message}'");
+                failedCallback.Invoke();
+                return;
+            }
+            
+            if (!string.IsNullOrEmpty(webRequest.error))
+            {
+                _log.Error($"[byte[]] Http 'post' request to url:'{url}' failed. err:'{webRequest.error}'");
+                failedCallback.Invoke();
+                return;
+            }
+
+            callback.Invoke(webRequest.downloadHandler.data);
+        }
+
+        /// <summary>
+        /// Http Post 请求。
+        /// </summary>
+        /// <param name="url">url</param>
+        /// <param name="callback">回调</param>
+        /// <param name="failedCallback">失败回调</param>
+        /// <param name="jsonStr">请求json数据</param>
+        /// <param name="headerData">请求头数据</param>
+        /// <param name="searchStrData">查询字符串数据</param>
+        public async void HttpPost(string url, Action<byte[]> callback, Action failedCallback,
+            string jsonStr = null, 
+            Dictionary<string, string> headerData = null, 
+            Dictionary<string, string> searchStrData = null)
+        {
+            url = GetUrlWithSearchStrData(url, searchStrData);
+            
+            var webRequest = UnityWebRequest.Post(url, jsonStr, "application/json");
             
             if (headerData != null)
                 foreach (var data in headerData)
@@ -111,6 +157,26 @@ namespace Engine.Scripts.Runtime.Net
             {
                 callback.Invoke(Encoding.UTF8.GetString(bytes));
             }, failedCallback, searchStrData, headerData);
+        }
+
+        /// <summary>
+        /// Http Post 请求。
+        /// </summary>
+        /// <param name="url">url</param>
+        /// <param name="callback">回调</param>
+        /// <param name="failedCallback">失败回调</param>
+        /// <param name="jsonStr">请求json数据</param>
+        /// <param name="headerData">请求头数据</param>
+        /// <param name="searchStrData">查询字符串数据</param>
+        public void HttpPost(string url, Action<string> callback, Action failedCallback,
+            string jsonStr = null, 
+            Dictionary<string, string> headerData = null, 
+            Dictionary<string, string> searchStrData = null)
+        {
+            HttpPost(url, bytes =>
+            {
+                callback.Invoke(Encoding.UTF8.GetString(bytes));
+            }, failedCallback, jsonStr, headerData, searchStrData);
         }
 
         /// <summary>
