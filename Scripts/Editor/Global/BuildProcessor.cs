cf1b9e8087f6d9465288b4a6131f69a94e9d546d
黎旭 <897313750@qq.com>
2024-08-19 18:44:34 +0800

    fix: 测试gm.dat修改

--- Scripts/Editor/Global/BuildProcessor.cs
+++ Scripts/Editor/Global/BuildProcessor.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
@@ -12,6 +13,18 @@ namespace Engine.Scripts.Editor.Global
         {
             //输出打包后的Android工程路径
             Debug.Log($"Build processor android proj path : '{report.summary.outputPath}'");
+            var metadataPath = $"{report.summary.outputPath}/unityLibrary/src/main/assets/bin/Data/Managed/Metadata/global-metadata.dat";
+            var targetPath = $"{report.summary.outputPath}/unityLibrary/src/main/assets/bin/Data/Managed/Metadata/global-metadata2.dat";
+
+            if (!File.Exists(metadataPath))
+            {
+                Debug.Log($"not find global-metadata.dat");
+                return;
+            }
+            
+            Debug.Log($"find global-metadata.dat");
+            
+            File.Move(metadataPath, targetPath);
         }
     }
 }
