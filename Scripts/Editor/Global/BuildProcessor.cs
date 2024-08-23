6677c88ea2ae67dd16e2e9e6d7c54ae98047bb74
黎旭 <897313750@qq.com>
2024-08-23 20:10:29 +0800

    fix：测试global-metadata加密

--- Scripts/Editor/Global/BuildProcessor.cs
+++ Scripts/Editor/Global/BuildProcessor.cs
@@ -12,7 +12,10 @@ namespace Engine.Scripts.Editor.Global
         {
             //输出打包后的Android工程路径
             Debug.Log($"Build processor android proj path : '{path}'");
+            
             var metadataPath = $"{path}/src/main/assets/bin/Data/Managed/Metadata/global-metadata.dat";
+            
+            // 伪装的文件位置
             var targetPath = $"{path}/src/main/assets/bin/Data/Managed/Resources/System.dll-resources.dat";
             
             if (!File.Exists(metadataPath))
@@ -22,15 +25,44 @@ namespace Engine.Scripts.Editor.Global
             }
             
             Debug.Log($"Find global-metadata.dat. Encrypt.");
-
+            
             var bytesReal = File.ReadAllBytes(metadataPath);
+            
+            // 假文件数据
             var bytesFake = new byte[bytesReal.Length];
 
             for (int i = 0; i < bytesReal.Length; i++)
             {
-                bytesFake[i] = (byte) (bytesReal[i] ^ (byte)(Random.Range(1, 254) & 0xFF));
-                bytesReal[i] = (byte) (bytesReal[i] ^ 0xFF);
+                // 假文件随机修改内容
+                if (i % 1024 == 0)
+                    bytesFake[i] = 0xFA;
+                else if (i % 1024 == 1)
+                    bytesFake[i] = 0xB1;
+                else if (i % 1024 == 2)
+                    bytesFake[i] = 0x1B;
+                else if (i % 1024 == 3)
+                    bytesFake[i] = 0xAF;
+                else
+                    bytesFake[i] = (byte) (bytesReal[i] ^ (byte)(Random.Range(1, 254) & 0xFF));
             }
+            
+            // 修改开头的FAB11BAF
+            bytesFake[0] = 0xFA;
+            bytesFake[1] = 0x4E;
+            bytesFake[2] = 0x1B;
+            bytesFake[3] = 0x50;
+            
+            for (int i = 0; i < bytesReal.Length; i+=4)
+            {
+                for (int j = 0; j < 4; j++)
+                    bytesReal[i + j] = (byte) (bytesReal[i + j] ^ 0xFF);
+            }
+
+            // 修改开头的FAB11BAF
+            bytesReal[0] = 0x23;
+            bytesReal[1] = 0xC5;
+            bytesReal[2] = 0xD9;
+            bytesReal[3] = 0x87;
 
             File.WriteAllBytes(targetPath, bytesReal);
             File.WriteAllBytes(metadataPath, bytesFake);
