a572b7fb0987ca3cff0bf7bc65bbd965d75a93d0
黎旭 <897313750@qq.com>
2024-08-30 20:01:45 +0800

    fix: 全局配置打包

--- Scripts/Runtime/Global/GlobalConfig.cs
+++ Scripts/Runtime/Global/GlobalConfig.cs
@@ -15,23 +15,23 @@ namespace Engine.Scripts.Runtime.Global
         public EGlobalConfigLoginType loginType;
 
         // 服务器手动选择
-        public bool isSelectServer => _flags[0];
+        public bool isSelectServer => flags[0];
         // 自动显示GM
-        public bool autoShowGM => _flags[1];
+        public bool autoShowGM => flags[1];
         // 设置界面是否能打开GM
-        public bool settingViewGM => _flags[2];
+        public bool settingViewGM => flags[2];
         // 自动显示调试
-        public bool autoShowDebug => _flags[3];
+        public bool autoShowDebug => flags[3];
         // 编辑器 防沉迷
-        public bool editorAntiAddiction => _flags[4];
+        public bool editorAntiAddiction => flags[4];
         // 包 防沉迷
-        public bool packageAntiAddiction => _flags[5];
+        public bool packageAntiAddiction => flags[5];
         // 包 防沉迷
-        public bool antiAddictionTestEnv => _flags[6];
+        public bool antiAddictionTestEnv => flags[6];
         
         public LogConfig logConfig;
         public EachNetConfig netConfig;
-        private bool[] _flags = new bool[7];
+        public bool[] flags = new bool[7];
 
         public void SetFlags(bool isSelectServer,
             bool autoShowGM,
@@ -41,13 +41,13 @@ namespace Engine.Scripts.Runtime.Global
             bool packageAntiAddiction,
             bool antiAddictionTestEnv)
         {
-            _flags[0] = isSelectServer;
-            _flags[1] = autoShowGM;
-            _flags[2] = settingViewGM;
-            _flags[3] = autoShowDebug;
-            _flags[4] = editorAntiAddiction;
-            _flags[5] = packageAntiAddiction;
-            _flags[6] = antiAddictionTestEnv;
+            flags[0] = isSelectServer;
+            flags[1] = autoShowGM;
+            flags[2] = settingViewGM;
+            flags[3] = autoShowDebug;
+            flags[4] = editorAntiAddiction;
+            flags[5] = packageAntiAddiction;
+            flags[6] = antiAddictionTestEnv;
         }
     }
 }
