81e6657f3301d3e90148d800bbc901e80ea8789d
LeeroyLin <897313750@qq.com>
2025-04-02 18:13:19 +0800

    fix: 增加Float和Transition界面层

--- Scripts/Runtime/Global/EViewLayer.cs
+++ Scripts/Runtime/Global/EViewLayer.cs
@@ -4,7 +4,9 @@
     {
         Main = 100,
         Normal = 200,
+        Float = 250,
         Alert = 300,
+        Transition = 350,
         Mask = 400,
         GM = 500,
         System = 600,
